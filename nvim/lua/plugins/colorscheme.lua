8a440ee0c2e2679cc764afb5d1c1ba11415745ce
RationalSkeptic <scharan.pro@gmail.com>
2025-04-13 17:18:32 +0530

    Added lazyVim config with dracula theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,38 +1,41 @@
 return {
-  -- GitHub Theme
-  {
-    "projekt0n/github-nvim-theme",
-    priority = 1000,
-    lazy = false,
-    config = function()
-      require("github-theme").setup({
-        -- Optional configuration
-      })
-    end,
-  },
-  -- Dracula Theme
-  {
-    "Mofiqul/dracula.nvim",
-    priority = 1000,
-    lazy = false,
-  },
-  -- Rose Pine Theme
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    priority = 1000,
-    lazy = false,
-    opts = {
-      variant = "main", -- Using the main variant, not moon
-      dark_variant = "main",
+    -- Dracula Theme
+    {
+        "Mofiqul/dracula.nvim",
+        priority = 1000,
+        lazy = false,
+        config = function()
+            require("dracula").setup({})
+            -- Set the colorscheme *after* setup
+            vim.cmd.colorscheme("dracula")
+        end,
     },
-  },
-  -- Configure LazyVim to use one of these themes
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      -- Set your default theme here
-      colorscheme = "dracula", -- or "dracula" or "rose-pine" or "tokyonight"
+    -- Rose Pine Theme
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        priority = 1000,
+        lazy = false,
+        opts = {
+            variant = "main",
+            dark_variant = "main",
+        },
     },
-  },
+    -- Nord Theme
+    {
+        "shaunsingh/nord.nvim",
+        priority = 1000,
+        lazy = false,
+        config = function()
+            -- You can uncomment this line if you want to set it as default
+            -- vim.cmd.colorscheme("nord")
+        end,
+    },
+    -- Disable LazyVim from setting the colorscheme automatically
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = function() end, -- prevents LazyVim from overriding your setup
+        },
+    },
 }
